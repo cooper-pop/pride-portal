30cdb8d0f7bfb0597efe04d808bf5c315fab90a0
cooper-pop <cooper@prideofthepond.com>
2026-03-31 17:18:28 -0500

    Update extract.js

--- api/extract.js
+++ api/extract.js
@@ -34,7 +34,7 @@ module.exports = async function handler(req, res) {
 
     const message = await client.messages.create({
       model: 'claude-haiku-4-5-20251001',
-      max_tokens: 8000,
+      max_tokens: 16000,
       messages: [{ role: 'user', content: [contentBlock, { type: 'text', text: prompt }] }]
     });
 
